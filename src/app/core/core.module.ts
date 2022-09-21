7c42c1bec0b27250ca5213594952782e0ff223b4
Sami NEFZI <sami.nefzi@gmail.com>
2022-09-21 11:44:17 +0200

    create module & launche optimisation

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [IconsModule, LoginModule, TemplatesModule, UiModule],
+})
+export class CoreModule {}
